ddef8180cd82ac7ecd95cf729af2de404dc2bec6
Sergey Bobrovich <siarhey1987@gmail.com>
2021-12-11 02:32:46 +0300

    Refactor MultiplicationTable

--- HomeWork/src/home_work_2/Loops/MultiplicationTable.java
+++ HomeWork/src/home_work_2/Loops/MultiplicationTable.java
@@ -1,16 +1,21 @@
 package home_work_2.Loops;
 
+// 1.5. Вывести таблицу умножения в консоль. В консоли должно получиться также как и на картинке (динозаврика рисовать не надо): https://www.dropbox.com/s/ibakfuppvy2w32g/multiplication_table.jpeg?dl=0
+
 public class MultiplicationTable {
     public static void main(String[] args) {
         int j = 2;
+
         for (int i = 1; i <= 10; i++) {
+            System.out.println();
+
             for (int k = j; k < 10; k++) {
                 System.out.printf("%d * %d = %d\t", k, i, k * i);
+
                 if ((k % 5 == 0 || k % 9 == 0) && i != 10) {
-                    System.out.println();
                     break;
                 }else if (i == 10 && k == 5){
-                    System.out.println("\n");
+                    System.out.println();
                     i = 1;
                     j = 6;
                     break;

9cb57d26b36b035ea0c88137502cb1fdfea50879
Sergey Bobrovich <siarhey1987@gmail.com>
2021-12-12 02:07:58 +0300

    Refactored ArrayIterator

--- HomeWork/src/home_work_2/arrays/ArrayIterator.java
+++ HomeWork/src/home_work_2/arrays/ArrayIterator.java
@@ -23,8 +23,12 @@ public class ArrayIterator {
     public static void print(int[] array) {
         System.out.println("Вывод массива:");
 
-        for (int i : array) {
-            System.out.print(i + " ");
+        for (int i = 0; i < array.length; i++) {
+            if (i != array.length - 1) {
+                System.out.print(array[i] + " ");
+            }else {
+                System.out.print(array[i]);
+            }
         }
 
         System.out.println();

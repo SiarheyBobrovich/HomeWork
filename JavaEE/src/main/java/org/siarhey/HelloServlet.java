e00079155e0da5f55b3ac60aaf4d6edce2567edb
Sergey Bobrovich <siarhey1987@gmail.com>
2022-02-19 15:35:09 +0300

    Added HelloServlet

--- JavaEE/src/main/java/org/siarhey/HelloServlet.java
+++ JavaEE/src/main/java/org/siarhey/HelloServlet.java
@@ -2,10 +2,22 @@ package org.siarhey;
 
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws IOException {
-        httpServletResponse.getWriter().print("netu");
+        String name = httpServletRequest.getParameter("name");
+        String surname = httpServletRequest.getParameter("surname");
+
+        PrintWriter writer = httpServletResponse.getWriter();
+
+        writer.println("<html>");
+        writer.println("<h1>");
+
+        writer.println("Hello " + name + " " + surname + ".");
+
+        writer.println("</h1>");
+        writer.println("</html>");
     }
 }
